fcc021ae30f02f23f8dde0790fb18ed5abf7e786
Renan Pinto <renanrnpinto@gmail.com>
2022-08-30 14:21:29 -0300

    Requisito 02

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       <div>Hello,TrybeWallet!</div>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Login />
+        </Route>
+        <Route path="/carteira">
+          <Wallet />
         </Route>
       </Switch>
     </div>
